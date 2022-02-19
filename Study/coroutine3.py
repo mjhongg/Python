2b255b71947687a7358c83de08991926377edb28
mjhongg <myeongji.hong.82@gmail.com>
2022-02-19 16:22:09 +0900

    coroutine

---
+++ Study/coroutine3.py
@@ -0,0 +1,36 @@
+'''
+def number_coroutine():
+    try:
+        while True:
+            x = (yield)
+            print(x, end=' ')
+    except GeneratorExit:
+        print()
+        print('코루틴 종료')
+
+co = number_coroutine()
+next(co)
+
+for i in range(20):
+    co.send(i)
+
+co.close()
+'''
+
+def sum_coroutine():
+    try:
+        total = 0;
+        while True:
+            x = (yield)
+            total += x
+    except RuntimeError as e:
+        print(e)
+        yield total
+
+co = sum_coroutine()
+next(co)
+            
+for i in range(20):
+    co.send(i)
+
+print(co.throw(RuntimeError, '예외로 코루틴 끝내기'))
